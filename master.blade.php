994a545063fb5ec38b440a28359e76c3b94e6409
Paladin Digital <admin@paladindigital.co.uk>
2017-02-06 16:48:00 +0000

    Add bootstrap from CDN.

--- master.blade.php
+++ master.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 @yield('head')
 @yield('meta')
 @yield('styles')
@@ -13,6 +15,8 @@
 @yield('widgets')
 </aside>
 @include('themes::paladindigital.laravel-bootstrap._footer')
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 @yield('scripts')
 </body>
 </html>

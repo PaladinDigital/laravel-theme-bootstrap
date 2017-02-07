f3ea95f95b0b62492250eb28ab4d6ef700d55ab6
Paladin Digital <admin@paladindigital.co.uk>
2017-02-07 14:23:37 +0000

    Extract bootstrap includes into seperate partials

--- fixed.blade.php
+++ fixed.blade.php
@@ -1,32 +1,35 @@
 <?php
 /*
  * Theme Name: Bootstrap
- * Template: Fixed
+ * Template: Master
  * Author: Paladin Digital
  */
 $theme = 'themes::paladindigital.laravel-bootstrap';
 
+// Allow nav type to be overridden from controllers.
+if (!isset($nav)) {
+    $nav = $theme . '._navbar';
+}
+
 ?><!DOCTYPE html>
 <html>
 <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    @yield('head')
-    @yield('meta')
-    @yield('styles')
+@include($theme . '.cdn._bs3head')
+@yield('head')
+@include($theme . '._meta')
+@yield('styles')
 </head>
 <body>
-@include($theme . '._nav')
+@include($nav)
 <div class="container">
     @yield('content')
     <aside>
         @yield('sidebar')
-        @yield('widgets')
+        @stack('widgets')
     </aside>
 </div>
 @include($theme . '._footer')
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-@yield('scripts')
+@include($theme . '.cdn._bs3foot')
+@stack('scripts')
 </body>
 </html>
--- master.blade.php
+++ master.blade.php
@@ -14,8 +14,7 @@ if (!isset($nav)) {
 ?><!DOCTYPE html>
 <html>
 <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+@include($theme . '.cdn._bs3head')
 @yield('head')
 @include($theme . '._meta')
 @yield('styles')
@@ -30,9 +29,7 @@ if (!isset($nav)) {
     </aside>
 </div>
 @include($theme . '._footer')
-    <script src="http://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+@include($theme . '.cdn._bs3foot')
 @stack('scripts')
 </body>
 </html>

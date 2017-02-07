eb6590cff2608cb84488327e4e8b91ce602df51e
Paladin Digital <admin@paladindigital.co.uk>
2017-02-07 09:55:02 +0000

    Default charset to UTF-8

--- master.blade.php
+++ master.blade.php
@@ -12,7 +12,7 @@ $theme = 'themes::paladindigital.laravel-bootstrap';
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 @yield('head')
-@yield('meta')
+@include($theme . '._meta')
 @yield('styles')
 </head>
 <body>

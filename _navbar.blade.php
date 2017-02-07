52e51a264c975ab37662d8168708a6793a27fcd9
Paladin Digital <admin@paladindigital.co.uk>
2017-02-07 13:30:30 +0000

    Use bootstrap 3 style nav sr button.

--- _navbar.blade.php
+++ _navbar.blade.php
@@ -1,6 +1,9 @@
 <nav class="navbar navbar-toggleable-md navbar-default fixed-top">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
     </button>
     <a class="navbar-brand" href="#">{{ $brand or config('app.name', 'Laravel Application') }}</a>
     <div class="collapse navbar-collapse" id="navbarCollapse">
